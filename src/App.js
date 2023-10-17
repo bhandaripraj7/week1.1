5557a4643113d3ea2d3daabac6f4cdba5a32d488
prajwal bhandari <bhandaripraj7@gmail.com>
2023-10-17 16:23:44 +0300

    react practice week1.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+/* Basic React Component Exercises
+These exercises are aimed to practice basic JSX use in a single component or in a single file. No need to import or
+ export anything yet.
+Exercise 2: Create a function called `getGreeting` that returns a greeting message saying ‘Welcome to the World 
+of React’. Use the function as JSX expression as follows:
+<p>{getGreeting()}</p>; */
+
 import React from 'react';
-import './App.css';
-function App() {
-  return <h1 align="center">Hello, React</h1>
+
+function getGreeting() {
+  return 'Welcome to the World of React';
 }
-export default App;
+
+function GreetingComponent() {
+  return (
+    <p>{getGreeting()}</p>
+  );
+}
+
+export default GreetingComponent;
+
+
+
 
